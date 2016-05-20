c3713b48a99642e6616c7b79f3c8fb1701ee5e2f
Brian Ke <brian.ke@silverlining-systems.com>
2016-05-20 15:59:30 +0800

    1.3c

--- getid_tool/setup.py
+++ getid_tool/setup.py
@@ -2,15 +2,16 @@
 from setuptools import setup
 
 setup(
-    name = 'getid_tool',
+    name = 'getid_tools',
     packages = ['getid_tool'],
     scripts = ['getid_tool/getid'],
-    version = '1.1',
+    version = '1.3c',
     description = 'SLS Get ID Tool',
-    author = 'SLS-BrianKe',
-    author_email = 'brian.ke@silverlining-systems.com',
+    long_description=open("README").read(),
+    author = 'sls.jarvis, SLS-BrianKe',
+    author_email = 'pkg-admin@silverlining-systems.com',
     url = 'https://github.com/SilverLiningSystems/testbench',
-    download_url = 'https://github.com/SilverLiningSystems/testbench',
-    keywords = ['GetID'],
-    classifiers = [],
+    download_url = 'https://github.com/SilverLiningSystems/testbench/archive/v1.1.tar.gz',
+    keywords = ['Calxeda SilverLining SLS'],
+    classifiers = ['License :: OSI Approved :: BSD License', 'Programming Language :: Python :: 2.7'],
 )
